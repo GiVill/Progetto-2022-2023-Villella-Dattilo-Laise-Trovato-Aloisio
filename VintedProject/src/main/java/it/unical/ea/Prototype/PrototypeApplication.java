48a95d8e600ad4b087c3c2e70db5529888f0606c
GiVill <116676033+GiVill@users.noreply.github.com>
2023-04-26 14:56:20 +0200

    Creazione progetto

---
+++ VintedProject/src/main/java/it/unical/ea/Prototype/PrototypeApplication.java
@@ -0,0 +1,13 @@
+package it.unical.ea.Prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeApplication.class, args);
+	}
+
+}
