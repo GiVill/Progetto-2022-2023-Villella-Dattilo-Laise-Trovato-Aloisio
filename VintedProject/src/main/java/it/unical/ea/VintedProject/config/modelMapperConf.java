454d6d661aaa408370e529b7ee58ee1c6270d119
Ris8 <umby9a@gmail.com>
2023-05-07 17:43:56 +0200

    Add i primi Controller

--- VintedProject/src/main/java/it/unical/ea/VintedProject/config/modelMapperConf.java
+++ VintedProject/src/main/java/it/unical/ea/VintedProject/config/modelMapperConf.java
@@ -1,9 +1,19 @@
 package it.unical.ea.VintedProject.config;
 
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class modelMapperConf {
 
-    //asdbuiadbuwaibdjwabdupawbduawpybduwpyia
+    @Bean
+    public ModelMapper modelMapper() {
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+        return new ModelMapper();
+    }
 
 }
