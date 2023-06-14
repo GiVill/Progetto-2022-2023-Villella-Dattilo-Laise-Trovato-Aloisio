b719d81bf733bfb75120999d38dfa7d32c9805ae
KevinLaise-0 <kevinlaise04@gmail.com>
2023-06-14 11:31:43 +0200

    Swagger

--- VintedProject/src/main/java/it/unical/ea/VintedProject/config/SecurityConfig.java
+++ VintedProject/src/main/java/it/unical/ea/VintedProject/config/SecurityConfig.java
@@ -24,6 +24,22 @@ public class SecurityConfig {
                 .csrf()
                     .disable()
                 .authorizeHttpRequests()
+                .requestMatchers(
+                        "/api/v1/auth/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/v2/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/index.html",
+                        "vintedProject-api/v1/users"
+                ).permitAll()
                     .anyRequest()
                         .authenticated()
         ;
