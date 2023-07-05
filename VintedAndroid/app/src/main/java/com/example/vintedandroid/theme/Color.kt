832205f970776d3e26819789fc3c1d63f3dadf81
Umberto Aloisio <57840350+Ris8TR@users.noreply.github.com>
2023-07-05 21:41:40 +0200

    fix -colori molto belli

--- VintedAndroid/app/src/main/java/com/example/vintedandroid/theme/Color.kt
+++ VintedAndroid/app/src/main/java/com/example/vintedandroid/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.vintedandroid.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Green80 = Color(0xFF9BE5B2)
-val GreenGrey80 = Color(0xFFA9B6A1)
-val LightGreen80 = Color(0xFFB7DDB8)
+val Green80 = Color(0xFF177245)
+val GreenGrey80 = Color(0xFF177245)
+val LightGreen80 = Color(0xFFBFFF00)
 
-val Green40 = Color(0xFF60C68A)
-val GreenGrey40 = Color(0xFF6D7D6C)
-val LightGreen40 = Color(0xFF82D6)
+val Green40 = Color(0xFF177245)
+val GreenGrey40 = Color(0xFF177245)
+val LightGreen40 = Color(0xFFBFFF00)
