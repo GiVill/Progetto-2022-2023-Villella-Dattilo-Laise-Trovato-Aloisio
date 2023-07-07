6827f1cad2e7b3447b0b5b342b11e3d87a00c26b
GiVill <vlllgu01e10m208q@studenti.unical.it>
2023-07-07 18:48:58 +0200

    Revert "Update InsertionController.java"
    This reverts commit 04914dd4997b1fcab8e4340c6728f3c12906f13e.

--- VintedProject/src/main/java/it/unical/ea/VintedProject/controller/InsertionController.java
+++ VintedProject/src/main/java/it/unical/ea/VintedProject/controller/InsertionController.java
@@ -29,10 +29,14 @@ public class InsertionController {
 
     @GetMapping("/insertions")
     //@PreAuthorize("hasAnyRole('user','admin')")
-    public ResponseEntity<Page<BasicInsertionDto>> all(@RequestParam("page") int page){
-
-        return ResponseEntity.ok(basicInsertionService.getAllPaged(page));
-
+    public ResponseEntity<Page<BasicInsertionDto>> all(Jwt jwt, @RequestParam("page") int page){
+        //utente pro
+        if (converter.extractResourceRoles(jwt).equals("ROLE_app_admin")){
+            return ResponseEntity.ok(basicInsertionService.getAllPaged(page));
+        }
+        else{
+            return ResponseEntity.ok(basicInsertionService.getAllByIsNormal(page));
+        }
     }
 
     @PostMapping("/insertions")
@@ -101,8 +105,8 @@ public class InsertionController {
 
 
     //potrebbe essere superflua
-    /*@GetMapping("/insertions/isNormal/{page}")
+    @GetMapping("/insertions/isNormal/{page}")
     public ResponseEntity<Page<BasicInsertionDto>> getByIsNormal(@PathVariable int page){
         return ResponseEntity.ok(basicInsertionService.getAllByIsNormal(page));
-    }*/
+    }
 }
